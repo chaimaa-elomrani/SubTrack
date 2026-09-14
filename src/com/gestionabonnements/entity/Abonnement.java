b7113e50aab99a4e0679f948ad951a212dcc2e38
chaimaelomrani <chaimaelomrani6@gmail.com>
2026-09-14 03:33:29 +0100

    created the main class abonnement

--- src/com/gestionabonnements/entity/Abonnement.java
+++ src/com/gestionabonnements/entity/Abonnement.java
@@ -1,4 +1,80 @@
 package com.gestionabonnements.entity;
 
-public class Abonnement {
+import java.time.LocalDate;
+import java.util.UUID;
+
+public abstract class Abonnement {
+    private String id;
+    private String nomService;
+    private double montantMensuel;
+    private LocalDate dateDebut;
+    private LocalDate dateFin;
+    private StatutAbonnement statut;
+
+
+    public Abonnement(String nomService, double montantMensuel, LocalDate dateDebut, LocalDate dateFin) {
+        this.id = UUID.randomUUID().toString();
+        this.nomService = nomService;
+        this.montantMensuel = montantMensuel;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.statut = StatutAbonnement.ACTIVE;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNomService() {
+        return nomService;
+    }
+
+    public void setNomService(String nomService) {
+        this.nomService = nomService;
+    }
+
+    public double getMontantMensuel() {
+        return montantMensuel;
+    }
+
+    public void setMontantMensuel(double montantMensuel) {
+        this.montantMensuel = montantMensuel;
+    }
+
+    public LocalDate getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(LocalDate dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public LocalDate getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(LocalDate dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public StatutAbonnement getStatut() {
+        return statut;
+    }
+
+    public void setStatut(StatutAbonnement statut) {
+        this.statut = statut;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Abonnement{" +
+                "id='" + id + '\'' +
+                ", nomService='" + nomService + '\'' +
+                ", montantMensuel=" + montantMensuel +
+                ", dateDebut=" + dateDebut +
+                ", dateFin=" + dateFin +
+                ", statut=" + statut +
+                '}';
+    }
 }
