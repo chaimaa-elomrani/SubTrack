861ff5beb386d2c8fe1223b69c9f45f005516526
chaimaelomrani <chaimaelomrani6@gmail.com>
2026-09-14 19:41:02 +0100

    added the logic of the method in the abonnement service , completed

--- src/com/gestionabonnements/service/AbonnementService.java
+++ src/com/gestionabonnements/service/AbonnementService.java
@@ -1,4 +1,48 @@
 package com.gestionabonnements.service;
 
+import com.gestionabonnements.dao.AbonnementDAO;
+import com.gestionabonnements.entity.Abonnement;
+import com.gestionabonnements.entity.StatutAbonnement;
+
+import java.util.List;
+import java.util.Optional;
+
 public class AbonnementService {
+
+    private AbonnementDAO abonnementDAO = new AbonnementDAO();
+
+    public void create(Abonnement abonnement) {
+        abonnementDAO.create(abonnement);
+    }
+
+    public Optional<Abonnement> findById(String id) {
+        return abonnementDAO.findById(id);
+    }
+    public List<Abonnement> findAll() {
+        return abonnementDAO.findAll();
+    }
+    public void update(Abonnement abonnement) {
+        abonnementDAO.update(abonnement);
+    }
+
+    public void delete(String id) {
+        abonnementDAO.delete(id);
+    }
+
+    public List<Abonnement> findActiveSubscriptions() {
+        return abonnementDAO.findActiveSubscriptions();
+    }
+
+    public List<Abonnement> findByType(String type) {
+        return abonnementDAO.findByType(type);
+    }
+
+    public void resilier(String id) {
+        Optional<Abonnement> opt = abonnementDAO.findById(id);
+        if (opt.isPresent()) {
+            Abonnement abonnement = opt.get();
+            abonnement.setStatut(StatutAbonnement.RESILIE);
+            abonnementDAO.update(abonnement);
+        }
+    }
 }
