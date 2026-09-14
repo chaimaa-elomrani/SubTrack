c901dfc470ac1138dd47b01b8a68f25edb080c4f
chaimaelomrani <chaimaelomrani6@gmail.com>
2026-09-14 03:39:16 +0100

    added the classes abonnement sans engagement and the class paiment

--- src/com/gestionabonnements/entity/AbonnementSansEngagement.java
+++ src/com/gestionabonnements/entity/AbonnementSansEngagement.java
@@ -1,4 +1,16 @@
 package com.gestionabonnements.entity;
 
-public class AbonnementSansEngagement {
+import java.time.LocalDate;
+
+public class AbonnementSansEngagement extends Abonnement {
+
+    public AbonnementSansEngagement(String nomService, double montantMensuel,
+                                    LocalDate dateDebut, LocalDate dateFin) {
+        super(nomService, montantMensuel, dateDebut, dateFin);
+    }
+
+    @Override
+    public String toString() {
+        return "AbonnementSansEngagement{" + super.toString() + '}';
+    }
 }
--- src/com/gestionabonnements/entity/Paiement.java
+++ src/com/gestionabonnements/entity/Paiement.java
@@ -1,4 +1,76 @@
 package com.gestionabonnements.entity;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class Paiement {
+
+    private String idPaiement;
+    private String idAbonnement;
+    private LocalDate dateEcheance;
+    private LocalDate datePaiement;
+    private String typePaiement;
+    private StatutPaiement statut;
+
+
+    public Paiement(String idAbonnement, LocalDate dateEcheance, String typePaiement) {
+        this.idPaiement = UUID.randomUUID().toString();
+        this.idAbonnement = idAbonnement;
+        this.dateEcheance = dateEcheance;
+        this.datePaiement = null;
+        this.typePaiement = typePaiement;
+        this.statut = StatutPaiement.NON_PAYE;
+    }
+
+    public String getIdPaiement() {
+        return idPaiement;
+    }
+
+    public String getIdAbonnement() {
+        return idAbonnement;
+    }
+
+    public LocalDate getDateEcheance() {
+        return dateEcheance;
+    }
+
+    public void setDateEcheance(LocalDate dateEcheance) {
+        this.dateEcheance = dateEcheance;
+    }
+
+    public LocalDate getDatePaiement() {
+        return datePaiement;
+    }
+
+    public void setDatePaiement(LocalDate datePaiement) {
+        this.datePaiement = datePaiement;
+    }
+
+    public String getTypePaiement() {
+        return typePaiement;
+    }
+
+    public void setTypePaiement(String typePaiement) {
+        this.typePaiement = typePaiement;
+    }
+
+    public StatutPaiement getStatut() {
+        return statut;
+    }
+
+    public void setStatut(StatutPaiement statut) {
+        this.statut = statut;
+    }
+
+    @Override
+    public String toString() {
+        return "Paiement{" +
+                "idPaiement='" + idPaiement + '\'' +
+                ", idAbonnement='" + idAbonnement + '\'' +
+                ", dateEcheance=" + dateEcheance +
+                ", datePaiement=" + datePaiement +
+                ", typePaiement='" + typePaiement + '\'' +
+                ", statut=" + statut +
+                '}';
+    }
 }
