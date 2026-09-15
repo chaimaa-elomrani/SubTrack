00753d7d1b3a39337550230e6f17d5d4de1b8d0a
chaimaelomrani <chaimaelomrani6@gmail.com>
2026-09-15 02:49:31 +0100

    finished crud and search in the abonnement service

--- src/com/gestionabonnements/service/AbonnementService.java
+++ src/com/gestionabonnements/service/AbonnementService.java
@@ -2,14 +2,18 @@ package com.gestionabonnements.service;
 
 import com.gestionabonnements.dao.AbonnementDAO;
 import com.gestionabonnements.entity.Abonnement;
+import com.gestionabonnements.entity.Paiement;
 import com.gestionabonnements.entity.StatutAbonnement;
+import com.gestionabonnements.dao.PaiementDAO;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public class AbonnementService {
 
     private AbonnementDAO abonnementDAO = new AbonnementDAO();
+    private PaiementDAO paiementDAO = new PaiementDAO();
 
     public void create(Abonnement abonnement) {
         abonnementDAO.create(abonnement);
@@ -36,13 +40,26 @@ public class AbonnementService {
     public List<Abonnement> findByType(String type) {
         return abonnementDAO.findByType(type);
     }
-
+    //cherche l’abonnement, change le statut en RESILIE, puis sauvegarde
     public void resilier(String id) {
         Optional<Abonnement> opt = abonnementDAO.findById(id);
         if (opt.isPresent()) {
-            Abonnement abonnement = opt.get();
+             Abonnement abonnement = opt.get();
             abonnement.setStatut(StatutAbonnement.RESILIE);
             abonnementDAO.update(abonnement);
         }
     }
+
+    public void genererEcheances(Abonnement abonnement) {
+        LocalDate date = abonnement.getDateDebut();
+        LocalDate fin = abonnement.getDateFin();
+
+        while (!date.isAfter(fin)) {
+            Paiement paiement = new Paiement(abonnement.getId(), date, "Carte");
+            paiementDAO.create(paiement);
+            date = date.plusMonths(1);
+        }
+    }
+
+
 }
